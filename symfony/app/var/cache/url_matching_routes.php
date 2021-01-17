89a59892cb51d610b277e3b42a2c7412a9436913
Malakaton <manelbermudez@gmail.com>
2021-01-18 00:55:30 +0100

    changed the endpoint name to get activities from itinerary

--- symfony/app/var/cache/url_matching_routes.php
+++ symfony/app/var/cache/url_matching_routes.php
@@ -13,16 +13,15 @@ return [
     [ // $regexpList
         0 => '{^(?'
                 .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
-                .'|/itineraries/([^/]++)(?'
-                    .'|(*:66)'
-                    .'|/activity(*:82)'
+                .'|/itineraries/([^/]++)/activity(?'
+                    .'|(*:75)'
                 .')'
             .')/?$}sD',
     ],
     [ // $dynamicRoutes
         35 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
-        66 => [[['_route' => 'itinerary_get_by_id', 'auth' => false, '_controller' => 'Academy\\ActivityItinerary\\EntryPoint\\Http\\Controller\\GetItineraryByIdController'], ['id'], ['GET' => 0, 'HEAD' => 1], null, false, true, null]],
-        82 => [
+        75 => [
+            [['_route' => 'activities_itinerary_get_by_id', 'auth' => false, '_controller' => 'Academy\\ActivityItinerary\\EntryPoint\\Http\\Controller\\GetActivitiesByItineraryUuidController'], ['id'], ['GET' => 0, 'HEAD' => 1], null, false, false, null],
             [['_route' => 'itinerary_store_activity', 'auth' => false, '_controller' => 'Academy\\ActivityItinerary\\EntryPoint\\Http\\Controller\\AddActivityItineraryController'], ['id'], ['POST' => 0], null, false, false, null],
             [null, null, null, null, false, false, 0],
         ],
